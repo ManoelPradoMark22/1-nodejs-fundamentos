9f3ccdf2ca3d92d7b9583499c20d2d388b243dd0
Manoel Prado <manoelprado.aecjr@gmail.com>
2022-04-06 19:32:40 -0300

    create node project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+  //return response.send("Hello World!");
+  return response.json({message: "Hello World IGNITE!"});
+})
+
+app.listen(3333);
