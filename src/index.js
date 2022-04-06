e044a4455ce6511e82685fd246c6d2d3f7386610
Manoel Prado <manoelprado.aecjr@gmail.com>
2022-04-06 19:43:36 -0300

    add nodemon

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  //return response.send("Hello World!!!");
   return response.json({message: "Hello World IGNITE!"});
 })
 
