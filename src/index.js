af8ea425705858971881fc36bb71f52b37892a4d
Manoel Prado <manoelprado.aecjr@gmail.com>
2022-04-07 12:43:26 -0300

    using http methods

--- src/index.js
+++ src/index.js
@@ -2,8 +2,36 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
+/**
+ * GET - Buscar uma informação dentro do servidor
+ * POST  - Inserir uma informação no servidor
+ * PUT - Alterar uma informação no servidor
+ * PATCH - Alterar uma informação específica
+ * DELETE - Deletar uma informação no servidor
+ */
+
+ app.get("/", (request, response) => {
   return response.json({message: "Hello World IGNITE!"});
+});
+
+app.get("/courses", (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses", (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+});
+
+app.delete("courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 3", "Curso 4"]);
 })
 
 app.listen(3333);
