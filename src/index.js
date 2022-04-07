4d5c34b0127d2a4f88fce07de1ef18b34ce61169
Manoel Prado <manoelprado.aecjr@gmail.com>
2022-04-07 14:11:09 -0300

    remove comments

--- src/index.js
+++ src/index.js
@@ -2,24 +2,7 @@ const express = require("express");
 
 const app = express();
 
-//para poder passar o body em Json
 app.use(express.json());
-
-/**
- * GET - Buscar uma informação dentro do servidor
- * POST  - Inserir uma informação no servidor
- * PUT - Alterar uma informação no servidor
- * PATCH - Alterar uma informação específica
- * DELETE - Deletar uma informação no servidor
- */
-
-/**
- * Tipos de parâmetros
- * 
- * Route Params => Identificar um recurso editar/deletar/burcar
- * Query Params => Paginação/Filtro
- * Body Params => Os objetos passados para inserção/alteração de recursos (JSON)
- */
 
  app.get("/", (request, response) => {
   return response.json({message: "Hello World IGNITE!"});
@@ -28,8 +11,6 @@ app.use(express.json());
 app.get("/courses", (request, response) => {
   const query = request.query;
   console.log(query);
-  //http://localhost:3333/courses?page=1
-  //http://localhost:3333/courses?page=1&type=2
   return response.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
@@ -40,8 +21,7 @@ app.post("/courses", (request, response) => {
 });
 
 app.put("/courses/:id", (request, response) => {
-  //const params = request.params;
-  const { id } = request.params; //com desestruturação
+  const { id } = request.params;
   console.log(id);
   return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
 });
